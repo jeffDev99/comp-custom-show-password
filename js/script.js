8ceb96404546de1bb0ccdbd353d959efbbbf279e
S.Mohammad.Amin.Jafarnezhad <83462403+jeffDev99@users.noreply.github.com>
2023-11-28 14:31:35 +0330

    add basic files

---
+++ js/script.js
@@ -0,0 +1,16 @@
+﻿ // Functions for select elems with id & class
+ var $ = document;
+ // Select Elems
+ var togglePassword = $.querySelector(".toggle-password");
+ var passwordField = $.querySelector("#password-field");
+
+ // Fire click event on eye icon
+ togglePassword.addEventListener("click", function () {
+   if (passwordField.type == "text") {
+     passwordField.type = "password";
+     togglePassword.classList.remove("active");
+   } else {
+     passwordField.type = "text";
+     togglePassword.classList.add("active");
+   }
+ })
